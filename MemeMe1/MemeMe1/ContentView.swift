5a39edee55bdf34e45670b32c8d85a78a1b5cbd1
Frank Feng <frank.feng@vendhq.com>
2020-01-22 17:42:31 +1300

    first commit

---
+++ MemeMe1/MemeMe1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemeMe1
+//
+//  Created by Frank Feng on 22/01/20.
+//  Copyright © 2020 vend. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
